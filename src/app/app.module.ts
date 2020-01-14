2be6f9a4bfac6e19f6a68e43fc19c0228d8559dc
Turcu, Stefanel <Stefanel.Turcu@centric.eu>
2020-01-14 23:44:47 +0200

    Query parser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule, MatButtonToggleModule, MatLabel, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule, MatTabsModule, MatDialogModule }  from "@angular/material";
+import {
+    MatCardModule,
+    MatButtonToggleModule,
+    MatLabel,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatDialogModule } from '@angular/material';
 import { QueryComponent } from './query/query.component';
 import { UploadComponent } from './upload/upload.component';
 import { XMLViewerDialogComponent } from './query/xml-viewer-dialog/xml-viewer-dialog.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgxSpinnerModule } from "ngx-spinner";
-import { SpinnerInterceptor } from "./services/spinner.interceptor"
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { SpinnerInterceptor } from './services/spinner.interceptor'
 
 @NgModule({
   declarations: [
@@ -26,7 +35,6 @@ import { SpinnerInterceptor } from "./services/spinner.interceptor"
     MatCardModule,
     MatButtonToggleModule,
     MatIconModule,
-    MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
     MatTabsModule,
--- src/app/query/query.component.ts
+++ src/app/query/query.component.ts
@@ -16,9 +16,74 @@ export class QueryComponent implements OnInit {
   title;
   content;
   items = []
+
+  query: string = '';
+  invalidInput = false;
+
+  // tslint:disable-next-line: max-line-length
+  regexQuery = new RegExp('(select where )(((tag [a-zA-Z]+ contains value [a-zA-Z]+ and )|(tree depth min [0-9]+ and)|(contains tag [a-zA-Z]+ and )|(size=[0-9]+ and ))*)((tag [a-zA-Z]+ contains value [a-zA-Z]+)|(tree depth min [0-9]+)|(contains tag [a-zA-Z]+)|(size=[0-9]+))');
+  regexTagValue = new RegExp('tag [a-zA-Z]+ contains value [a-zA-Z]+');
+  regexDepth = new RegExp('tree depth min [0-9]+');
+  regexTag = new RegExp('contains tag [a-zA-Z]+');
+  regexSize = new RegExp('size=[0-9]+');
+
   constructor(public dialog: MatDialog, public httpService: XMLHttpService, public spinner: NgxSpinnerService) { }
 
   ngOnInit() {
+  }
+
+  public parseQuery() {
+    if (!this.regexQuery.test(this.query)) {
+      this.invalidInput = true;
+      return;
+    }
+
+    const text =  this.query.substring(13);
+    const items = text.split('and');
+
+    items.forEach(item => {
+      const value = item.trim();
+
+      if (this.regexTagValue.test(value)) {
+        this.callTagValue(value);
+      }
+
+      if (this.regexDepth.test(value)) {
+        this.callDepth(value);
+      }
+
+      if (this.regexTag.test(value)) {
+        this.callTag(value);
+      }
+
+      if (this.regexSize.test(value)) {
+        this.callSize(value);
+      }
+    });
+  }
+
+  callTagValue(query: string) {
+    const params = query.split(' ');
+    console.log(params);
+  }
+
+  callDepth(query: string) {
+    const params = query.split(' ');
+    console.log(params);
+  }
+
+  callTag(query: string) {
+    const params = query.split(' ');
+    this.httpService.getDocumentByTag(params[2]).subscribe((tag => {
+      this.items = tag.docs;
+      return tag;
+    }));
+    console.log(params);
+  }
+
+  callSize(query: string) {
+    const params = query.split('=');
+    console.log(params);
   }
 
   openDialog(): void {
@@ -29,16 +94,20 @@ export class QueryComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result)
+      console.log(result);
     });
   }
 
   makeRequest() {
     console.log('A request has been made.');
-    this.httpService.getDocumentByTag('hello').subscribe((tag => {   
+    this.httpService.getDocumentByTag('hello').subscribe((tag => {
       this.items = tag.docs;
-      return tag;}),
-      ); 
+      return tag;
+    }));
   }
 
+  changeQuery(event) {
+    this.invalidInput = false;
+    this.query = event.srcElement.value;
+  }
 }
